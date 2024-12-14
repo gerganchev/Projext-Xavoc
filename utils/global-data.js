3883f13d1c382665a421c54150e151ee1b0b0e3f
gerganchev <dude_in_love@abv.bg>
2024-12-14 19:58:33 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'ZEON';
+    : '$BONE';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Zeon AI';
+    : 'ByteOne AI';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved';
