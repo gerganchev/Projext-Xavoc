4ac8a6b41602ceffe529327f79d1f54b9a98c47b
gerganchev <dude_in_love@abv.bg>
2024-12-14 17:23:06 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,14 @@
-import Head from 'next/head';
 import Link from 'next/link';
 
-export default function Header() {
+export default function Header({ name }) {
   return (
-    <>
-      <Head>
-        <title>Project Xavoc</title> {/* Static Title */}
-      </Head>
-
-      <header className="pt-20 pb-12">
-        <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
-        <p className="text-2xl text-center dark:text-white">
-          <Link href="/">
-            My Website
-          </Link>
-        </p>
-      </header>
-    </>
+    <header className="pt-20 pb-12">
+      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <p className="text-2xl text-center dark:text-white">
+        <Link href="/">
+          {name}
+        </Link>
+      </p>
+    </header>
   );
 }
