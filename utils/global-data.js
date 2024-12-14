d0a308c363b3c6f013caef2ce862e278580ffcec
gerganchev <dude_in_love@abv.bg>
2024-12-14 18:12:57 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,4 +1,7 @@
 export const getGlobalData = () => {
+  const name = process.env.BLOG_NAME
+    ? decodeURI(process.env.BLOG_NAME)
+    : 'XAV';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Project Xavoc';
