c9b07dd98934bce7fd43672e96dedb22b279ef54
gerganchev <dude_in_love@abv.bg>
2024-12-14 17:14:18 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function Header({ Project Xavoc }) {
+export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
